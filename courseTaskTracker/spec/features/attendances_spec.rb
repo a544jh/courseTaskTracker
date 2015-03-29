2481ced26037d39258e04bd5eea111deea2874e0
a544jh <axel.wikstrom@gmail.com>
2015-03-29 19:10:01 +0300

    leave course test

--- courseTaskTracker/spec/features/attendances_spec.rb
+++ courseTaskTracker/spec/features/attendances_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe "Attendance" do
 	let!(:user) { FactoryGirl.create :user }
 	let!(:course1) { FactoryGirl.create :course, name:"Käjä" }
-	
+
 	before :each do
 		visit signin_path
 		fill_in('username', with:'Pekka')
 		fill_in('password', with:'Foobar1')
 		click_button('Log in')
 	end
-	
+
 	it "user can join course" do
 		visit course_path(course1)
 		expect {
@@ -19,5 +19,15 @@ describe "Attendance" do
 		expect(user.courses.count).to eq(1)
 		expect(course1.users.count).to eq(1)
 		expect(page).to have_button "Leave course"
+	end
+
+	it "user can leave course" do
+		a = Attendance.create(user_id:user.id, course_id:course1.id)
+		expect(user.courses.count).to eq(1)
+		expect(course1.users.count).to eq(1)
+		visit course_path(course1)
+		expect {
+			click_button("Leave course")
+		}.to change{Attendance.count}.from(1).to(0)
 	end
 end
