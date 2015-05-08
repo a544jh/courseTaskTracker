28c31f547a836341ef3897be2d1a52aeebeda32b
a544jh <axel.wikstrom@gmail.com>
2015-05-08 13:51:10 +0300

    more tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -15,5 +15,14 @@ describe "User" do
       expect(page).to have_content 'Welcome back!'
       expect(page).to have_content 'Pekka'
     end
+
+    it "can not signin with wrong credentials" do
+      visit signin_path
+      fill_in('username', with:'Pekka')
+      fill_in('password', with:'Foobar11')
+      click_button('Log in')
+
+      expect(page).to have_content 'mismatch'
+    end
   end
 end
